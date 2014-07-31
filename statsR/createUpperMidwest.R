e26d12182716812b483371008a15b7fb51ce07a4
Christopher Paciorek <paciorek@scf.Berkeley.EDU>
2014-07-31 10:05:01 -0700

    lots of edits to createUpperMidwest.R to deal with mapping to taxa common names, adding x-y albers coords, selecting settlement pollen based on RC data

--- statsR/createUpperMidwest.R
+++ statsR/createUpperMidwest.R
@@ -1,11 +1,16 @@
-# PLS data
+#### Code for preparing Upper Midwest PLS and pollen datasets for Camp Peon 2014
+#### Chris Paciorek and Simon Goring
+#### July 2014
 
-# download westerncompv0.3.csv from Wiki
+### PLS data
+
+## preparatory step: download westerncompv0.3.csv from Wiki
 
 dat <- read.csv('westerncompv0.3.csv')
 
 coreTaxa <- c('ash', 'basswood', 'beech', 'birch', 'black.gum.sweet.gum', 'cedar.juniper', 'cherry', 'dogwood', 'elm', 'fir', 'hemlock', 'hickory', 'ironwood', 'maple', 'oak', 'pine', 'poplar.tulip.poplar', 'spruce', 'tamarack' , 'walnut', 'other.hardwood')
 
+## omit some columns (bald cypress because we have no "other conifer" category
 omit <- c("bald.cypress", "no.tree", "unknown")
 
 taxa <- names(dat)
@@ -14,15 +19,15 @@ taxa <- taxa[!taxa %in% omit]
 
 dat <- dat[ , c('x', 'y', taxa)]
 
+## combine other hardwood taxa
 other <- taxa[!taxa %in% coreTaxa]
 taxa <- taxa[!taxa %in% other]
-
 dat[ , "other.hardwood"] <- dat[ , "other.hardwood"] + rowSums(dat[ , other])
 dat <- dat[ , c('x', 'y', taxa)]
 
 write.table(dat, file = 'pls.csv', quote = FALSE, sep = ",", row.names = FALSE)
 
-# get pollen data
+### Pollen data from Neotoma
 
 #  Simon Goring - July 25, 2014
 #
@@ -41,23 +46,81 @@ towerLoc <- c(-(86 + 2/60 + 13/3600), 46 + 32/60 + 31/3600)
 
 
 library(neotoma)
+library(rgdal)
+library(neotoma)
 
 #  These are the sites around UNDERC and Tower Hill Lake.
 #  This gives us 46 records (on 25 July, 2014)
-bounding.box <- c(-92, 44, -85, 48)
+boundingBox <- c(-92, 44, -85, 48)
 
-box.sites <- get_dataset(loc = bounding.box, datasettype = 'pollen')
+boxSites <- get_dataset(loc = boundingBox, datasettype = 'pollen')
 
-all.sites <- get_download(sapply(box.sites, function(x)x$DatasetID))
+allSites <- get_download(sapply(boxSites, function(x)x$DatasetID))
 
-all.compiled <- lapply(all.sites, compile_taxa, list.name = 'P25')
+allCompiled <- lapply(allSites, compile_taxa, list.name = 'P25')
 
-all.data.frame <- compile_downloads(all.compiled)
+allData <- compile_downloads(allCompiled)
 
+coordinates(allData) <- c("long", "lat")
+proj4string(allData) <- CRS("+proj=longlat")
+tmp <- spTransform(allData, CRSobj=CRS('+init=epsg:3175'))
 
+pollen <- as.data.frame(allData)
+pollen$x <- coordinates(tmp)[ , 1]
+pollen$y <- coordinates(tmp)[ , 2]
+
+transTable <- read.csv('pollenTranslation.csv', stringsAsFactors = FALSE)
+
+omit <- transTable$latin[transTable$common == 'omit']
+
+colnames <- names(pollen)
+pollen <- pollen[ , colnames[!colnames %in% omit]]
+
+others <- transTable$latin[transTable$common == "other.hardwood"]
+
+pollen$other.hardwood <- rowSums(pollen[ , others], na.rm = TRUE)
+
+pollen <- pollen[ , !names(pollen) %in% others] 
+
+toConvert <- names(pollen)[names(pollen) %in% transTable$latin]
+
+for(nm in toConvert) {
+  common <- transTable$common[transTable$latin == nm]
+  pollen[[nm]][is.na(pollen[[nm]])] <- 0
+  if(!common %in% names(pollen)) {
+    pollen[[common]] <- pollen[[nm]]
+  } else {
+    pollen[[common]] <- pollen[[common]] + pollen[[nm]]
+  }
+  pollen[nm] <- NULL
+}
+  
 #  write.csv(all.data.frame, 'data/all_site_df.csv')
 
+pollenBySite <- split(pollen, pollen$sitename)
+
+getSettlement <- function(x) {
+  x <- x[x$age > 1950 - 1830 & x$age < 1950 - 1700, ]
+  if(nrow(x)) {
+    wh <- which.min(abs(x$age - (1950 - 1800)))
+    return(x[wh, ])
+  } else return(NULL)
+}
+
+getSettlement_oldVersion <- function(x) {
+  settlementDate <- 1950 - 1820
+  wh <- which.min(abs(x$age - settlementDate))
+  if(abs(x$age[wh] - settlementDate) < 50)
+    return(x[wh, ]) else return(NULL)
+}
+
+settlementPollen <- sapply(pollenBySite, getSettlement)
+settlementPollen <- do.call(rbind, settlementPollen)
+
+## older code from Simon
+
 #  We need to get the chroncontrol tables so we can assign pre-settlement:
+if(FALSE) {
 all.chronIDs <- sapply(all.sites, function(x)x$sample.meta$ChronologyID[1])
 
 chron.tables <- lapply(all.chronIDs, function(x)try(get_chroncontrol(x)))
@@ -65,5 +128,5 @@ chron.tables <- lapply(all.chronIDs, function(x)try(get_chroncontrol(x)))
 find_european <- function(x){
   settlement <- which(x$chron.control$ControlType == 'European')
 }
+}
 
-set <- lapply(chron.tables, find_european)
