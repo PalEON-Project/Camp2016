5bc9bd214de1e95286cf7611fa08f538ca296b7d
Christopher Paciorek <paciorek@scf.Berkeley.EDU>
2014-08-10 17:38:16 -0700

    updated rings.csv

--- statsR/createRingData.R
+++ statsR/createRingData.R
@@ -4,8 +4,8 @@ trees <- read.csv('../PecanInputs/H 2012 Adult Field Data.csv', stringsAsFactors
 trees$DBH11[trees$DBH11 == "missing data"] <- NA
 trees$DBH12[trees$DBH12 == "missing data"] <- NA
 
-trees$DBH11 <- as.numeric(trees$DBH11) / 10  # now in mm dbh
-trees$DBH12 <- as.numeric(trees$DBH12) / 10
+trees$DBH11 <- as.numeric(trees$DBH11) * 10  # now in mm dbh
+trees$DBH12 <- as.numeric(trees$DBH12) * 10
 
 
 source('matchInventoryRings.R')
@@ -15,6 +15,10 @@ rings <- Read_Tuscon("../PecanInputs/Revised 2/")
       
  ## Match observations & format for JAGS
 combined <- matchInventoryRings(trees,rings,nyears=30,coredOnly=TRUE)
+
+ringCols <- 16:ncol(combined)
+# transform to mm of diameter not radius
+combined[ , ringCols] <- 2 * combined[ , ringCols]
 
 # restrict to (0, 15) x (0, 15) as that seems to be where sampling is concentrated
 combined <- combined[combined$X > 0 & combined$X < 15 & combined$Y < 15 & combined$Y > 0, ]
