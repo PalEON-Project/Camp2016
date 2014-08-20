9524dc73cdc3f82bdb50e4d8cb85face7ad70147
Simon <simon.j.goring@gmail.com>
2014-08-20 09:55:34 -0500

    Revisions to the UP_Pollen.R file to reflect changes in the neotoma package and to remove the European Settlement identification.

--- Neotoma_Pollen/R/UP_Pollen.R
+++ Neotoma_Pollen/R/UP_Pollen.R
@@ -11,27 +11,24 @@
 #      c. lat/long and albers x/y 
 #      d. taxa as counts (or proportions?)
 
-library(neotoma)
+install.packages("devtools") 
+require(devtools) 
+install_github("neotoma", "SimonGoring") 
+require(neotoma) 
+library(rgdal)
+library(ggplot2)
 
-#  These are the sites around UNDERC and Tower Hill Lake.
-#  This gives us 46 records (on 25 July, 2014)
-bounding.box <- c(-92, 44, -85, 48)
+#  This is the explicit location of Tower Hill Lake, that we're not using, but whatever.
+towerLoc <- c(-(86 + 2/60 + 13/3600), 46 + 32/60 + 31/3600)
 
-box.sites <- get_dataset(loc = bounding.box, datasettype = 'pollen')
+##  These are the sites around UNDERC and Tower Hill Lake.
+##  This gives us 46 records (on 25 July, 2014)
+boundingBox <- c(-92, 44, -85, 48)
 
-all.sites <- get_download(sapply(box.sites, function(x)x$DatasetID))
+boxSites <- get_dataset(loc = boundingBox, datasettype = 'pollen')
 
-all.compiled <- lapply(all.sites, compile_taxa, list.name = 'P25')
+allSites <- get_download(boxSites)
 
-all.data.frame <- compile_downloads(all.compiled)
+allCompiled <- compile_taxa(allSites, list.name = 'P25')
 
-#  write.csv(all.data.frame, 'Neotoma_Pollen/data/output/all_site_df.csv')
-
-#  We need to get the chroncontrol tables so we can assign pre-settlement:
-all.chronIDs <- sapply(all.sites, function(x)x$sample.meta$ChronologyID[1])
-
-chron.tables <- lapply(all.chronIDs, function(x)try(get_chroncontrol(x)))
-
-find_european <- function(x){
-  settlement <- which(x$chron.control$ControlType == 'European')
-}
+allData <- compile_downloads(allCompiled)
